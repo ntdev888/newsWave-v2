f7a1d7850a1a3a31950ad629311b8a45d62d982a
Nic <tauroa.nic@gmail.com>
2024-09-16 17:24:40 +1200

    backend for commenting

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
-from .views import RegisterView, LoginView, LogoutView, UpdateSettingsView, UpdateTopicsView, UserDetailView
+from .views import RegisterView, get_user_id, LoginView, LogoutView, UpdateSettingsView, UpdateTopicsView, UserDetailView
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    path('login/', LoginView.as_view(), name='login'),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+    # path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('settings/', UpdateSettingsView.as_view(), name='update_settings'),
     path('topics/', UpdateTopicsView.as_view(), name='update_topics'),
     path('me/', UserDetailView.as_view(), name='user_detail'),
+    path('get-user-id/', get_user_id, name='get-user-id'),
 ]
--- users/views.py
+++ users/views.py
@@ -8,6 +8,7 @@ from rest_framework.views import APIView
 from rest_framework import status
 from django.contrib.auth import authenticate, login, logout
 from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 
 # Registration View
 class RegisterView(generics.CreateAPIView):
@@ -28,6 +29,21 @@ class LoginView(APIView):
             token, created = Token.objects.get_or_create(user=user)
             return Response({'token': token.key}, status=status.HTTP_200_OK)
         return Response({'error': 'Invalid Credentials'}, status=status.HTTP_400_BAD_REQUEST)
+    
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_user_id(request):
+    username = request.query_params.get('username', None)
+    if username is not None:
+        try:
+            user = User.objects.get(username=username)
+            return Response({'user_id': user.id}, status=status.HTTP_200_OK)
+        except User.DoesNotExist:
+            return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
+    return Response({'error': 'Username not provided'}, status=status.HTTP_400_BAD_REQUEST)
+
+
 
 # Logout View
 class LogoutView(APIView):
